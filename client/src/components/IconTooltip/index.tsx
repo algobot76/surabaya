17d3562c706706050d659ab50cbcbb97e341c942
MayKikuchi <48613270+MayKikuchi@users.noreply.github.com>
2020-06-05 16:14:55 -0700

    Add icon tooltips and island component (#17)
    * start tooltips
    
    * tooltips functional
    
    * make iconWidth constant
    
    * made fields in icontooltips
    
    * overlayed tooltips on randomized island image
    
    * remove -1 from randomize islands
    
    * use enum for IconType
    
    * center icons better

---
+++ client/src/components/IconTooltip/index.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+import RightBoat from "../../assets/icons/boat right.png";
+import Volcano from "../../assets/icons/volcano.png";
+import Factory from "../../assets/icons/factory.png";
+import Deciduous from "../../assets/icons/deciduous.png";
+import DeciduousMultiple from "../../assets/icons/deciduousMultiples.png";
+import Evergreen from "../../assets/icons/evergreen.png";
+import EvergreenMultiple from "../../assets/icons/evergreenMultiples.png";
+import Pond from "../../assets/icons/pond.png";
+import PondMultiple from "../../assets/icons/pondMultiples.png";
+import Rock from "../../assets/icons/rock.png";
+import RockMultiple from "../../assets/icons/rockMultiples.png";
+import styles from "./styles.module.css";
+import styled from "styled-components";
+
+interface IconToolTipProps {
+  type: IconType;
+  data: any;
+}
+
+export enum IconType {
+  String,
+  StringMultiple,
+  Boolean,
+  BooleanMultiple,
+  Int,
+  IntMultiple,
+  Other,
+  OtherMultiple,
+  Import,
+  Method,
+  Constructor,
+}
+
+function getIcon(type: IconType): string {
+  switch (type) {
+    case IconType.Import:
+      return RightBoat;
+    case IconType.Constructor:
+      return Volcano;
+    case IconType.Method:
+      return Factory;
+    case IconType.Boolean:
+      return Deciduous;
+    case IconType.BooleanMultiple:
+      return DeciduousMultiple;
+    case IconType.Int:
+      return Pond;
+    case IconType.IntMultiple:
+      return PondMultiple;
+    case IconType.String:
+      return Evergreen;
+    case IconType.StringMultiple:
+      return EvergreenMultiple;
+    case IconType.Other:
+      return Rock;
+    case IconType.OtherMultiple:
+      return RockMultiple;
+    default:
+      return "";
+  }
+}
+
+const MarginDiv = styled.div`
+  margin-top: 2px;
+  margin-bottom: 4px;
+  font-size: 12px;
+`;
+
+function getToolTipText(props: IconToolTipProps) {
+  const { data } = props;
+  switch (props.type) {
+    case IconType.Import:
+      return <MarginDiv>Imports: 'ADD IMPORT DATA HERE'</MarginDiv>;
+    case IconType.Constructor:
+    case IconType.Method:
+      const parameterString =
+        data.parameters &&
+        Object.keys(data.parameters)
+          ?.map((p) => `${p}: ${data.parameters[p]}`)
+          ?.join(", ");
+      return (
+        <div>
+          <MarginDiv>Name: {data.name}</MarginDiv>
+          <MarginDiv>Access modifier: {data.accessModifier}</MarginDiv>
+          <MarginDiv>Parameters: {parameterString || "none"}</MarginDiv>
+          <MarginDiv>Return type: {data.returnType}</MarginDiv>
+        </div>
+      );
+    case IconType.Boolean:
+    case IconType.BooleanMultiple:
+    case IconType.Int:
+    case IconType.IntMultiple:
+    case IconType.String:
+    case IconType.StringMultiple:
+    case IconType.Other:
+    case IconType.OtherMultiple:
+      return (
+        <div>
+          <MarginDiv>Name: {data.name}</MarginDiv>
+          <MarginDiv>Type: {data.type}</MarginDiv>
+          <MarginDiv>Access modifier: {data.accessModifier}</MarginDiv>
+        </div>
+      );
+    default:
+      return null;
+  }
+}
+
+// IMPORTANT!! iconWidth is a constant because it is also used in ToolTipSquare for calculating the ArrayContainer width
+export const iconWidth = 30;
+
+const SizedIconContainer = styled.div`
+  width: ${iconWidth}px;
+  height: ${iconWidth}px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SizedImage = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+const IconToolTip: React.FC<IconToolTipProps> = (props: IconToolTipProps) => {
+  const icon = getIcon(props.type);
+  const toolTipText = getToolTipText(props);
+  return (
+    <div className={styles.tooltip}>
+      <SizedIconContainer>
+        <SizedImage src={icon} />
+      </SizedIconContainer>
+      <span className={styles.tooltiptext}>{toolTipText}</span>
+    </div>
+  );
+};
+
+export default IconToolTip;
---
+++ client/src/components/Island/index.tsx
@@ -0,0 +1,214 @@
+import React from "react";
+import IconToolTip, { IconType, iconWidth } from "../IconTooltip";
+import styled from "styled-components";
+
+import Island1 from "../../assets/islands/island1.png";
+import Island2 from "../../assets/islands/island2.png";
+import Island3 from "../../assets/islands/island3.png";
+import Island4 from "../../assets/islands/island4.png";
+import Island5 from "../../assets/islands/island5.png";
+
+const islandArray = [Island1, Island2, Island3, Island4, Island5];
+
+const mockClass = {
+  name: "c1",
+  type: "Interface",
+  accessModifier: "private",
+  lineCount: 100,
+  imports: ["ex2", "ex3"],
+  fields: {
+    string: [
+      {
+        name: "field1",
+        type: "String",
+        accessModifier: "public",
+      },
+    ],
+    boolean: [
+      {
+        name: "field1",
+        type: "Boolean",
+        accessModifier: "public",
+      },
+    ],
+    other: [
+      {
+        name: "field3",
+        type: "Object",
+        accessModifier: "private",
+      },
+    ],
+  },
+  methods: [
+    {
+      name: "method1",
+      accessModifier: "private",
+      parameters: { param1: "String", param2: "int" },
+      returnType: "void",
+    },
+    {
+      name: "method1",
+      accessModifier: "private",
+      parameters: { param1: "String", param2: "int" },
+      returnType: "void",
+    },
+    {
+      name: "method1",
+      accessModifier: "private",
+      parameters: { param1: "String", param2: "int" },
+      returnType: "void",
+    },
+    {
+      name: "method1",
+      accessModifier: "private",
+      parameters: { param1: "String", param2: "int" },
+      returnType: "void",
+    },
+  ],
+  constructors: [
+    {
+      name: "ex1",
+      accessModifier: "public",
+      parameters: { param1: "String", param2: "int" },
+      returnType: null,
+    },
+  ],
+};
+
+function getToolTipDataArray(classObject: any) {
+  const methods = classObject["methods"];
+  const constructors = classObject["constructors"];
+  const fields = classObject["fields"];
+  const strings = fields?.["string"];
+  const stringMultiples = fields?.["stringMultiples"];
+  const booleans = fields?.["boolean"];
+  const booleanMultiples = fields?.["booleanMultiples"];
+  const ints = fields?.["int"];
+  const intMultiples = fields?.["intMultiples"];
+  const other = fields?.["other"];
+  const otherMultiples = fields?.["otherMultiples"];
+
+  const methodToolTips =
+    methods?.map((m, index) => (
+      <IconToolTip key={`method_${index}`} type={IconType.Method} data={m} />
+    )) || [];
+  const constructorToolTips =
+    constructors?.map((c, index) => (
+      <IconToolTip
+        key={`constructor_${index}`}
+        type={IconType.Constructor}
+        data={c}
+      />
+    )) || [];
+  const stringToolTips =
+    strings?.map((s, index) => (
+      <IconToolTip key={`string_${index}`} type={IconType.String} data={s} />
+    )) || [];
+  const stringMultiplesToolTips =
+    stringMultiples?.map((sm, index) => (
+      <IconToolTip
+        key={`stringMultiple_${index}`}
+        type={IconType.StringMultiple}
+        data={sm}
+      />
+    )) || [];
+  const booleanToolTipsToolTips =
+    booleans?.map((b, index) => (
+      <IconToolTip key={`boolean_${index}`} type={IconType.Boolean} data={b} />
+    )) || [];
+  const booleanMultiplesToolTips =
+    booleanMultiples?.map((bm, index) => (
+      <IconToolTip
+        key={`booleanMultiple_${index}`}
+        type={IconType.BooleanMultiple}
+        data={bm}
+      />
+    )) || [];
+  const intToolTipsToolTips =
+    ints?.map((i, index) => (
+      <IconToolTip key={`int_${index}`} type={IconType.Int} data={i} />
+    )) || [];
+  const intMultiplesToolTips =
+    intMultiples?.map((im, index) => (
+      <IconToolTip
+        key={`intMultiple_${index}`}
+        type={IconType.IntMultiple}
+        data={im}
+      />
+    )) || [];
+  const otherToolTipsToolTips =
+    other?.map((o, index) => (
+      <IconToolTip key={`other_${index}`} type={IconType.Other} data={o} />
+    )) || [];
+  const otherMultiplesToolTips =
+    otherMultiples?.map((om, index) => (
+      <IconToolTip
+        key={`otherMultiple_${index}`}
+        type={IconType.OtherMultiple}
+        data={om}
+      />
+    )) || [];
+
+  return [
+    ...methodToolTips,
+    ...constructorToolTips,
+    ...stringToolTips,
+    ...stringMultiplesToolTips,
+    ...booleanToolTipsToolTips,
+    ...booleanMultiplesToolTips,
+    ...intToolTipsToolTips,
+    ...intMultiplesToolTips,
+    ...otherToolTipsToolTips,
+    ...otherMultiplesToolTips,
+  ];
+}
+
+const IslandContainer = styled.div<{ minWidth }>`
+  width: ${(props) => `${props.minWidth}px`};
+  height: ${(props) => `${props.minWidth}px`};
+  position: relative;
+  display: flex;
+  justify-content: center;
+`;
+
+const IslandImage = styled.img<{ maxWidth }>`
+  max-width: ${(props) => `${props.maxWidth}px`};
+  max-height: ${(props) => `${props.maxWidth}px`};
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const ToolTipSquare = styled.div<{ width }>`
+  width: ${(props) => `${props.width}px`};
+  height: ${(props) => `${props.width}px`};
+  z-index: 9;
+  position: relative;
+  top: 40%;
+  -webkit-transform: translateY(-40%);
+  transform: translateY(-40%);
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Island: React.FC = () => {
+  const toolTipArray = getToolTipDataArray(mockClass);
+  const numberOfIcons = toolTipArray.length;
+  const square = Math.sqrt(numberOfIcons);
+  const numberHorizontal = Math.ceil(square);
+  const width = numberHorizontal * iconWidth;
+
+  const minimumInnerIslandWidth = width + iconWidth * 3;
+  const numberOfIslandImages = islandArray.length;
+  const randomIslandIndex = Math.floor(Math.random() * numberOfIslandImages);
+  const islandImage = islandArray[randomIslandIndex];
+
+  return (
+    <IslandContainer minWidth={minimumInnerIslandWidth}>
+      <ToolTipSquare width={width}>{toolTipArray}</ToolTipSquare>
+      <IslandImage src={islandImage} maxWidth={minimumInnerIslandWidth} />
+    </IslandContainer>
+  );
+};
+
+export default Island;
