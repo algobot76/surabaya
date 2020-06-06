2e08add90b05a36207221709d0fc885240ddf96c
Dara Nguyen <duongnt1204@gmail.com>
2020-06-05 20:35:40 -0700

    added legend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./App.module.css";
 import Island from "./components/Island";
+import Legend from "./components/Legend";
 
 function App() {
   return (
     <div className={styles.App}>
-      <div className={styles.leftSide} />
+      <div className={styles.leftSide}>
+        <Legend></Legend>
+      </div>
+
       <div className={styles.rightSide}>
+        <Island></Island>
+        <Island></Island>
         <Island></Island>
       </div>
     </div>
