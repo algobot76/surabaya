c4f990043ab91d5308329331cbebc8f13d61b5ac
Kaitian Xie <xkaitian@gmail.com>
2020-06-05 19:45:24 -0700

    Add AccessModifier enum

---
+++ src/main/java/com/github/algobot76/surabaya/util/AccessModifier.java
@@ -0,0 +1,12 @@
+package com.github.algobot76.surabaya.util;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public enum AccessModifier {
+
+	@JsonProperty("public")
+	PUBLIC, @JsonProperty("private")
+	PRIVATE, @JsonProperty("protected")
+	PROTECTED
+
+}
--- src/main/java/com/github/algobot76/surabaya/util/Class.java
+++ src/main/java/com/github/algobot76/surabaya/util/Class.java
@@ -14,7 +14,7 @@ public class Class {
 	private final String type;
 
 	@JsonProperty("access_modifier")
-	private final String access_modifier;
+	private final AccessModifier access_modifier;
 
 	@JsonProperty("line_count")
 	private final int lineCount;
