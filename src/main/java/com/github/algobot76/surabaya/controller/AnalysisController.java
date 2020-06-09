8bb4d200dd6fe84ebfe2d5da450f2687bf86a003
Kaitian Xie <xkaitian@gmail.com>
2020-06-08 19:56:10 -0700

    Update analysis endpoint

--- src/main/java/com/github/algobot76/surabaya/controller/AnalysisController.java
+++ src/main/java/com/github/algobot76/surabaya/controller/AnalysisController.java
@@ -1,37 +1,39 @@
 package com.github.algobot76.surabaya.controller;
 
-import com.github.algobot76.surabaya.util.Class;
-import com.github.algobot76.surabaya.util.Package;
-import com.github.algobot76.surabaya.util.File;
-import com.github.algobot76.surabaya.util.*;
+import com.github.algobot76.surabaya.service.storage.StorageService;
+import com.github.algobot76.surabaya.util.Analyzer;
+import com.github.algobot76.surabaya.util.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.Optional;
 
 @RestController
 public class AnalysisController {
 
+	private final StorageService storageService;
+
+	@Autowired
+	public AnalysisController(StorageService storageService) {
+		this.storageService = storageService;
+	}
+
 	@GetMapping("/analysis")
 	public Project analysis() {
-		Project project = new Project();
-		File file1 = new File();
-		file1.addImport("import1");
-		Class class1 = new Class("Class1", "Interface", "public", 100);
-		class1.addField(new Field("foo", "String", "public"));
-		class1.addField(new Field("bar", "boolean", "private"));
-		Method method1 = new Method("method1", "public", "void");
-		method1.addParameter(new Parameter("a", "int"));
-		method1.addParameter(new Parameter("b", "String"));
-		Method method2 = new Method("method2", "public", "int");
-		method2.addParameter(new Parameter("x", "String"));
-		class1.addMethod(method1);
-		class1.addMethod(method2);
-		Constructor constructor1 = new Constructor("Class1", "public");
-		constructor1.addParameter(new Parameter("z", "double"));
-		class1.addConstructor(constructor1);
-		Package package1 = project.getOrCreatePackage("package1");
-		package1.addFile(file1);
-		file1.addClass(class1);
-		return project;
+
+		Optional<Path> path = storageService.loadAll().max(Comparator.naturalOrder());
+
+		if (path.isEmpty()) {
+			return new Project();
+		}
+
+		Resource resource = storageService.loadAsResource(path.get().toString());
+		Analyzer analyzer = new Analyzer();
+		return analyzer.analyze(resource);
 	}
 
 }
