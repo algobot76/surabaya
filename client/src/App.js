11135851f4f887a03aeb3eb9c324a5f26868f0c2
Kaitian Xie <xkaitian@gmail.com>
2020-06-09 19:15:36 -0700

    Remove mock data (#67)
    * Remove mock data
    
    * Fetch data from analysis endpoint
    
    * Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import styles from "./App.module.css";
-import Island from "./components/Island";
 import Legend from "./components/Legend";
 import ImportPanel from "./components/ImportPanel";
-import mockFile from "./mockFile.json";
+import Island from "./components/Island";
+
+const Package = (props) => {
+  const { content } = props;
+  return (
+    <Fragment>
+      {content.files.map((file, index) => (
+        <Island key={index} fileAnalysis={file} />
+      ))}
+    </Fragment>
+  );
+};
 
 function App() {
+  const [data, setData] = useState({});
+
   return (
     <div className={styles.App}>
       <div className={styles.leftSide}>
         <Legend />
-        <ImportPanel />
+        <ImportPanel setData={setData} />
       </div>
-
       <div className={styles.rightSide}>
-        <Island fileAnalysis={mockFile} />
-        <Island fileAnalysis={mockFile} />
-        <Island fileAnalysis={mockFile} />
+        {data.packages &&
+          Object.entries(data.packages).map((entry, index) => {
+            const [packageName, packageContent] = entry;
+            return <Package key={index} content={packageContent} />;
+          })}
       </div>
     </div>
   );
