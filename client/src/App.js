c1dd9716bc4e7cabe5ded1e9e1422834c74a5c31
Kaitian Xie <xkaitian@gmail.com>
2020-06-09 18:31:37 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import styles from "./App.module.css";
 import Legend from "./components/Legend";
 import ImportPanel from "./components/ImportPanel";
+import Island from "./components/Island";
+
+const Package = (props) => {
+  const { content } = props;
+  return (
+    <Fragment>
+      {content.files.map((file, index) => (
+        <Island key={index} fileAnalysis={file} />
+      ))}
+    </Fragment>
+  );
+};
 
 function App() {
   const [data, setData] = useState({});
@@ -12,7 +24,13 @@ function App() {
         <Legend />
         <ImportPanel setData={setData} />
       </div>
-      <div className={styles.rightSide}></div>
+      <div className={styles.rightSide}>
+        {data.packages &&
+          Object.entries(data.packages).map((entry, index) => {
+            const [packageName, packageContent] = entry;
+            return <Package key={index} content={packageContent} />;
+          })}
+      </div>
     </div>
   );
 }
