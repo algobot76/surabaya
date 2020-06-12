4a2dfad24dfea54052a979b9296cb93d591250bd
John Zou <extreme2202020@gmail.com>
2020-06-12 15:11:39 -0700

    Changed to using toString, removed manual file reading

--- src/main/java/com/github/algobot76/surabaya/util/Analyzer.java
+++ src/main/java/com/github/algobot76/surabaya/util/Analyzer.java
@@ -1,7 +1,6 @@
 package com.github.algobot76.surabaya.util;
 
 import com.github.algobot76.surabaya.SurabayaApplication;
-import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.visitor.VoidVisitor;
@@ -12,16 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.DirectoryIteratorException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
 
 public class Analyzer {
 
@@ -31,7 +21,7 @@ public class Analyzer {
 
 	private Project project;
 
-	private StaticAnalysisVisitor staticAnalysisVisitor;
+	private VoidVisitor<Project> staticAnalysisVisitor;
 
 	public Analyzer() {
 		project = new Project();
@@ -40,29 +30,18 @@ public class Analyzer {
 
 	public Project analyze(Resource file) {
 		FileUnzipper.unzip(file, destpath);
-		JavaParser parser = new JavaParser();
-		Path path = CodeGenerationUtils.mavenModuleRoot(SurabayaApplication.class).resolve("unzipped");
-		try (Stream<Path> paths = Files.walk(path)) {
-			paths.forEach(p -> {
-				if (p.toString().endsWith(".java")) {
-					try {
-						String content = Files.readString(p, StandardCharsets.US_ASCII);
-						ParseResult result = parser.parse(content);
-						CompilationUnit cu = (CompilationUnit) result.getResult().get();
-						String[] lines = content.lines().toArray(String[]::new);
-						staticAnalysisVisitor.setFileSrc(lines);
-						staticAnalysisVisitor.visit(cu, project);
-					}
-					catch (IOException e) {
-						logger.error(e.getMessage());
-					}
-				}
-			});
+		SourceRoot sourceRoot = new SourceRoot(
+				CodeGenerationUtils.mavenModuleRoot(SurabayaApplication.class).resolve("unzipped"));
+		try {
+			List<ParseResult<CompilationUnit>> results = sourceRoot.tryToParse();
+			for (ParseResult result : results) {
+				CompilationUnit cu = (CompilationUnit) result.getResult().get();
+				staticAnalysisVisitor.visit(cu, project);
+			}
 		}
 		catch (IOException e) {
 			logger.error(e.getMessage());
 		}
-
 		return project;
 	}
 
