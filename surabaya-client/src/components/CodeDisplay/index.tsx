a63e255c057228350e2698ac1cbc5455ddd49497
john-zou <61474884+john-zou@users.noreply.github.com>
2020-06-13 17:31:32 -0700

    Update styling (#113)

--- surabaya-client/src/components/CodeDisplay/index.tsx
+++ surabaya-client/src/components/CodeDisplay/index.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "codemirror/lib/codemirror.css";
-import "codemirror/theme/xq-light.css";
+import "codemirror/theme/ttcn.css";
 import "codemirror/mode/clike/clike";
 import { Controlled as CodeMirror } from "react-codemirror2";
 import "./CodeDisplay.css";
-import { code } from "../../atoms";
+import { code as codeAtom } from "../../atoms";
 import { useRecoilValue } from "recoil";
 
 const CodeDisplay = () => {
-  const _code = useRecoilValue(code);
+  const code = useRecoilValue(codeAtom);
   return (
     <div className="code-display">
       <CodeMirror
         onBeforeChange={() => {}}
-        value={_code}
+        value={code}
         options={{
           mode: "text/x-java",
-          theme: "xq-light",
+          theme: "ttcn",
           lineNumbers: true,
         }}
       />
